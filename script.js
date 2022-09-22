aebd61c1c0eb413134b206d121f70a153fc5d20d
Courtney Gibson <97132573+CourtG@users.noreply.github.com>
2022-09-22 15:23:57 -0400

    added animated scroll icon, created new project template, added signature

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navlist = document.getElementById('navList');
-
-hamburger.addEventListener('click', () => {
-    navlist.classList.toggle('show');
-});
